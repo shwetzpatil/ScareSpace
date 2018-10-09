d341859695786bbfa4eb9e9526f18cfedefebdc2
Robbie Campbell <roberto251983@gmail.com>
2018-10-09 17:18:23 +0100

    added add space and dislplay space function

--- App/Model/Space.rb
+++ App/Model/Space.rb
@@ -1,16 +1,26 @@
 require 'pg'
-require_relative 'database_connection'
+require_relative 'Database_connection'
+require_relative 'User'
 
 class Space
+  attr_reader :id, :name, :address, :price, :description, :lister_id
 
-  def initialize()
+  def initialize(id:, name:, address:, price:, description:, lister_id:)
+    @id = id
+    @name = name
+    @address = address
+    @price = price
+    @description = description
+    @lister_id = lister_id
   end
 
   def self.all
-    # result = DatabaseConnection.select("*", "spaces", "name", "robbie" )
-    # connection = PG.connect(dbname: 'scarespace')
-    # all_spaces = connection.exec "SELECT * FROM spaces"
     result = DatabaseConnection.all("spaces")
     result.map { |row| row['name']}
+  end
+
+  def self.create(id:, name:, address:, price:, description:, lister_id:)
+    result = DatabaseConnection.insert("spaces", "id, name, address, price, description, lister_id", "'#{id}', '#{name}', '#{address}','#{price}', '#{description}', '#{lister_id}'", "id, name, address, price, description, lister_id")
+    Space.new(id: result[0]['id'], name: result[0]['name'], address: result[0]['address'], price: result[0]['price'], description: result[0]['description'], lister_id: result[0]['lister_id'])
   end
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -9,6 +9,12 @@ describe Space do
   end
 
   describe '.create' do
+    it 'should create a new space and add it to the database' do
+      new_user = User.create(email: 'test@example.com', password: 'password123')
+      new_space = Space.create(id: 1, name: 'robbie', address: 'xyz', price: '100', description: 'abc', lister_id: new_user.id)
+      expect(new_space.name).to eq 'robbie'
+      expect(new_space).to respond_to :id
+    end
   end
   describe '.update' do
   end
@@ -16,9 +22,8 @@ describe Space do
   end
   describe '.all' do
     it 'should display all spaces' do
-      # space1 = @connection.exec("INSERT INTO spaces (name) VALUES ('robbie');")
       DatabaseConnection.insert("spaces", "name", "'robbie'", "name")
-    
+
       expect(Space.all).to include("robbie")
 
     end
