fb70e4b1e3d621debdacb031cd62db96cf17f382
Robbie Campbell <roberto251983@gmail.com>
2018-10-09 12:04:21 +0100

    front end framework barebones complete

--- App/Controller/app.rb
+++ App/Controller/app.rb
@@ -8,6 +8,23 @@ class ScareSpace < Sinatra::Base
     # "Welcome to ScareSpace"
   end
 
+  get '/signup' do
+    erb :signup
+  end
+
+  get '/spaces' do
+    erb :spaces
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  get '/requests' do
+    erb :requests
+  end
+  
+
   get '/' do
     "ScareSpace"
   end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,39 +4,39 @@ feature 'Testing infrastructure' do
     expect(page).to have_content 'Welcome to ScareSpace'
   end
 
-  scenario 'takes you to a sign up page' do 
+  scenario 'takes you to a sign up page' do
     visit('/homepage')
-    expect(page).to have_content 'Sign Up'
+    expect(page).to have_selector(:link_or_button, 'Sign Up')
     click_button('Sign Up')
-    expect(page)to have_content 'Enter email'
-    expect(page)to have_content 'Enter password'
-  end 
+    expect(page).to have_content 'Enter email'
+    expect(page).to have_content 'Enter password'
+  end
 
-  scenario 'takes you to a log in page' do 
+  scenario 'takes you to a log in page' do
     visit('/homepage')
-    expect(page).to have_content 'Log In'
+    expect(page).to have_selector(:link_or_button, 'Log In')
     click_button('Log In')
-    expect(page)to have_content 'Log In page'
-  end 
+    expect(page).to have_content 'Log In page'
+  end
 
-  scenario 'takes you to a spaces page' do 
+  scenario 'takes you to a spaces page' do
     visit('/homepage')
-    expect(page).to have_content 'Create new space'
+    expect(page).to have_selector(:link_or_button, 'Create new space')
     click_button('Create new space')
-    expect(page)to have_content 'Create a space'
-  end 
+    expect(page).to have_content 'Create a space'
+  end
 
-  scenario 'takes you to a request page' do 
+  scenario 'takes you to a request page' do
     visit('/homepage')
-    expect(page).to have_content 'Request Update'
+    expect(page).to have_selector(:link_or_button, 'Request Update')
     click_button('Request Update')
-    expect(page)to have_content 'User update page'
-  end 
+    expect(page).to have_content 'Current requests'
+  end
 
-  scenario 'logs you out of the page' do 
+  scenario 'logs you out of the page' do
     visit('/homepage')
-    expect(page).to have_content 'Log Out'
+    expect(page).to have_selector(:link_or_button, 'Log Out')
     click_button('Log Out')
-    expect(page)to have_content 'Log In'
-  end 
+    expect(page).to have_selector(:link_or_button, 'Log In')
+  end
 end
