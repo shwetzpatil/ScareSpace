7b4cb9536ba7be09290ffe52119cb06365976b77
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-10-10 14:50:09 +0100

    more refactoring

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -2,11 +2,11 @@ describe Space do
 
   before(:each) do
     @new_user = User.create(email: 'test@example.com', password: 'password123')
+    @new_space = Space.create(name: 'robbiespace', address: 'xyz', price: '100', description: 'abc', lister_id: @new_user.id)
   end
 
   describe '.all' do
     it 'should display all spaces' do
-      new_space = Space.create(name: 'robbiespace', address: 'xyz', price: '100', description: 'abc', lister_id: @new_user.id)
       spaces = Space.all
 
       names = spaces.map(&:name)
@@ -20,16 +20,14 @@ describe Space do
   # on create expect address to be unique
   describe '.create' do
     it 'should create a new space and add it to the database' do
-      new_space = Space.create(name: 'robbiespace', address: 'xyz', price: '100', description: 'abc', lister_id: @new_user.id)
-      expect(new_space.name).to eq 'robbiespace'
-      expect(new_space).to respond_to :id
+      expect(@new_space.name).to eq 'robbiespace'
+      expect(@new_space).to respond_to :id
     end
   end
 
   describe '.update' do
     it 'should enable a user to update the name of their space' do
-      new_space = Space.create(name: 'robbiespace', address: 'xyz', price: '100', description: 'abc', lister_id: @new_user.id)
-      Space.update(new_space.id, name: 'robbiescaryspace')
+      Space.update(@new_space.id, name: 'robbiescaryspace')
       space_list = Space.all
       names = space_list.map(&:name)
       expect(names).to include 'robbiescaryspace'
@@ -39,9 +37,7 @@ describe Space do
 
   describe '.find' do
     it 'finds a space' do
-      new_space = Space.create(name: 'robbiespace', address: 'xyz', price: '100', description: 'abc', lister_id: @new_user.id)
-      expected_space = Space.find(new_space.id)
-
+      expected_space = Space.find(@new_space.id)
       expect(expected_space.name).to eq"robbiespace"
       expect(expected_space.price).to eq "100.00"
 
@@ -50,8 +46,7 @@ describe Space do
   
   describe '.delete' do
     it 'should delete a space from the database based on name and user id' do
-      new_space = Space.create(name: 'robbiespace', address: 'xyz', price: '100', description: 'abc', lister_id: @new_user.id)
-      Space.delete(new_space.id)
+      Space.delete(@new_space.id)
       spaces = Space.all
       name = spaces.map(&:name)
       expect(name).not_to include("robbiespace")
